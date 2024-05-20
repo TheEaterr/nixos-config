136a1fd72df82306924952fe664fc1e187298762
Paul Breuil <tykapl.breuil@gmail.com>
2024-05-20 13:31:24 +0200

    initial commit

---
+++ nixos/configuration.nix
@@ -0,0 +1,280 @@
+# This is your system's configuration file.
+# Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  options,
+  ...
+}: {
+  # You can import other NixOS modules here
+  imports = [
+    # If you want to use modules your own flake exports (from modules/nixos):
+    # outputs.nixosModules.example
+
+    # Or modules from other flakes (such as nixos-hardware):
+    # inputs.hardware.nixosModules.common-cpu-amd
+    # inputs.hardware.nixosModules.common-ssd
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./users.nix
+
+    # Import your generated (nixos-generate-config) hardware configuration
+    ./hardware-configuration.nix
+    inputs.home-manager.nixosModules.home-manager
+    # ./nix-alien.nix
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # Add overlays your own flake exports (from overlays and pkgs dir):
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+
+      # You can also add overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
+  };
+
+  nix = let
+    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+  in {
+    settings = {
+      # Enable flakes and new 'nix' command
+      experimental-features = "nix-command flakes";
+      # Opinionated: disable global registry
+      flake-registry = "";
+      # Workaround for https://github.com/NixOS/nix/issues/9574
+      nix-path = config.nix.nixPath;
+    };
+    # Opinionated: disable channels
+    channel.enable = false;
+
+    # Opinionated: make flake registry and nix path match flake inputs
+    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+  };
+
+  home-manager = {
+    extraSpecialArgs = { inherit inputs outputs; };
+    users = {
+      # Import your home-manager configuration
+      eaterr = import ../home-manager/home.nix;
+    };
+  };
+
+  boot.kernelPackages = pkgs.linuxPackages_latest; 
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+
+  # Set your time zone.
+  time.timeZone = "Europe/Paris";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fr_FR.UTF-8";
+    LC_IDENTIFICATION = "fr_FR.UTF-8";
+    LC_MEASUREMENT = "fr_FR.UTF-8";
+    LC_MONETARY = "fr_FR.UTF-8";
+    LC_NAME = "fr_FR.UTF-8";
+    LC_NUMERIC = "fr_FR.UTF-8";
+    LC_PAPER = "fr_FR.UTF-8";
+    LC_TELEPHONE = "fr_FR.UTF-8";
+    LC_TIME = "fr_FR.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "fr";
+    xkbVariant = "oss_nodeadkeys";
+  };
+
+  # Configure console keymap
+  console.keyMap = "fr";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.eaterr = {
+    isNormalUser = true;
+    description = "Paul Breuil";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      git
+      chezmoi
+      micro
+      steam-run
+      kate
+    #  thunderbird
+    ];
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    git
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+  ];
+
+  programs.nix-ld.enable = true;
+  ## If needed, you can add missing libraries here. nix-index-database is your friend to
+  ## find the name of the package from the error message:
+  ## https://github.com/nix-community/nix-index-database
+  programs.nix-ld.libraries = options.programs.nix-ld.libraries.default ++ (with pkgs; [ 
+    stdenv.cc.cc
+       openssl
+       xorg.libXcomposite
+       xorg.libXtst
+       xorg.libXrandr
+       xorg.libXext
+       xorg.libX11
+       xorg.libXfixes
+       libGL
+       libva
+       xorg.libxcb
+       xorg.libXdamage
+       xorg.libxshmfence
+       xorg.libXxf86vm
+       libelf
+       
+       # Required
+       glib
+       gtk2
+       gtk3
+       bzip2
+       
+       # Without these it silently fails
+       xorg.libXinerama
+       xorg.libXcursor
+       xorg.libXrender
+       xorg.libXScrnSaver
+       xorg.libXi
+       xorg.libSM
+       xorg.libICE
+       gnome2.GConf
+       nspr
+       nss
+       cups
+       libcap
+       SDL2
+       libusb1
+       dbus-glib
+       ffmpeg
+       # Only libraries are needed from those two
+       libudev0-shim
+       
+       # Verified games requirements
+       xorg.libXt
+       xorg.libXmu
+       libogg
+       libvorbis
+       SDL
+       SDL2_image
+       glew110
+       libidn
+       tbb
+       
+       # Other things from runtime
+       flac
+       freeglut
+       libjpeg
+       libpng
+       libpng12
+       libsamplerate
+       libmikmod
+       libtheora
+       libtiff
+       pixman
+       speex
+       SDL_image
+       SDL_ttf
+       SDL_mixer
+       SDL2_ttf
+       SDL2_mixer
+       libappindicator-gtk2
+       libdbusmenu-gtk2
+       libindicator-gtk2
+       libcaca
+       libcanberra
+       libgcrypt
+       libvpx
+       librsvg
+       xorg.libXft
+       libvdpau
+       gnome2.pango
+       cairo
+       atk
+       gdk-pixbuf
+       fontconfig
+       freetype
+       dbus
+       alsaLib
+       expat
+       # Needed for electron
+       libdrm
+       mesa
+       libxkbcommon
+   ]);
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "23.11";
+}
