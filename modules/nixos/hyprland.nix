018daef6b0db34cac09f21f05a0c8bf42337e593
Paul Breuil <tykapl.breuil@gmail.com>
2024-05-30 22:08:04 +0200

    add gtk portal

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -11,6 +11,11 @@
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
 
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+
   environment.systemPackages = with pkgs; [
     pyprland
     hyprpicker
