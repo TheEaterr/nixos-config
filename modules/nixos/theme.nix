ffcffd6da66c0d69a816e4cb1120535048670522
Paul Breuil <tykapl.breuil@gmail.com>
2024-05-31 00:16:29 +0200

    remove console theming

--- modules/nixos/theme.nix
+++ modules/nixos/theme.nix
@@ -1,28 +1,6 @@
 { pkgs, ... }:
 
 {
-  console = {
-    earlySetup = true;
-    colors = [
-      "24273a"
-      "ed8796"
-      "a6da95"
-      "eed49f"
-      "8aadf4"
-      "f5bde6"
-      "8bd5ca"
-      "cad3f5"
-      "5b6078"
-      "ed8796"
-      "a6da95"
-      "eed49f"
-      "8aadf4"
-      "f5bde6"
-      "8bd5ca"
-      "a5adcb"
-    ];
-  };
-
   # Override packages
   nixpkgs.config.packageOverrides = pkgs: {
     discord = pkgs.discord.override {
