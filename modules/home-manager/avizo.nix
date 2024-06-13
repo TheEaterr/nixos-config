e13d13032527ab6e40bc2fe18d712fdf9b3164db
Paul Breuil <tykapl.breuil@gmail.com>
2024-06-13 23:53:24 +0200

    tweak avizo color

--- modules/home-manager/avizo.nix
+++ modules/home-manager/avizo.nix
@@ -8,7 +8,7 @@
     default = {
       background = "rgba(${config.scheme.base01-rgb-r}, ${config.scheme.base01-rgb-g}, ${config.scheme.base01-rgb-b}, 0.6)";
       border-color = "#${config.theme.base16Accent}";
-      bar-fg-color = "#${config.scheme.base06}";
+      bar-fg-color = "#${config.theme.base16Accent}";
       border-width = 2;
     };
   };
