8e8d4460a4bed26584aa98cdff5738b7d9d7b383
Paul Breuil <tykapl.breuil@gmail.com>
2024-06-15 19:46:01 +0200

    fix build

--- home-manager/maoune.nix
+++ home-manager/maoune.nix
@@ -15,15 +15,6 @@
     outputs.homeManagerModules.theme
   ];
 
-  # add garbage collection
-  nix.settings.auto-optimise-store = true;
-  nix.optimise.automatic = true;
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 14d";
-  };
-
   nix = let
     flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
   in {
@@ -34,6 +25,7 @@
       flake-registry = "";
       # Workaround for https://github.com/NixOS/nix/issues/9574
       nix-path = config.nix.nixPath;
+      auto-optimise-store = true;
     };
     # Opinionated: disable channels
     channel.enable = false;
@@ -41,6 +33,14 @@
     # Opinionated: make flake registry and nix path match flake inputs
     registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
     nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+
+    # Add gc settings
+    optimise.automatic = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 14d";
+    };
   };
 
   home = {
