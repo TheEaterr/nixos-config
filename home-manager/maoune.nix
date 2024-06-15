43a28c805a040707b859ba989551a9cac76ae4a8
Paul Breuil <tykapl.breuil@gmail.com>
2024-06-15 19:42:22 +0200

    add gc and flakes experimental to home manager

--- home-manager/maoune.nix
+++ home-manager/maoune.nix
@@ -15,6 +15,34 @@
     outputs.homeManagerModules.theme
   ];
 
+  # add garbage collection
+  nix.settings.auto-optimise-store = true;
+  nix.optimise.automatic = true;
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
+  };
+
+  nix = let
+    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+  in {
+    settings = {
+      # Enable flakes and new 'nix' command
+      experimental-features = "nix-command flakes";
+      # Opinionated: disable global registry
+      flake-registry = "";
+      # Workaround for https://github.com/NixOS/nix/issues/9574
+      nix-path = config.nix.nixPath;
+    };
+    # Opinionated: disable channels
+    channel.enable = false;
+
+    # Opinionated: make flake registry and nix path match flake inputs
+    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+  };
+
   home = {
     username = "eaterr";
     homeDirectory = "/home/eaterr";
