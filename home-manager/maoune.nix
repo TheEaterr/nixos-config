eff16835a6837b4db54833007eb71c52c1a88aca
Paul Breuil <tykapl.breuil@gmail.com>
2024-06-15 19:46:28 +0200

    fix for home manager

--- home-manager/maoune.nix
+++ home-manager/maoune.nix
@@ -27,9 +27,6 @@
       nix-path = config.nix.nixPath;
       auto-optimise-store = true;
     };
-    # Opinionated: disable channels
-    channel.enable = false;
-
     # Opinionated: make flake registry and nix path match flake inputs
     registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
     nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
