6f62016b46f50d20d4d78dbd798dbe229809a718
Paul Breuil <tykapl.breuil@gmail.com>
2024-10-10 16:01:42 +0200

    remove avizo borders

--- modules/home-manager/avizo.nix
+++ modules/home-manager/avizo.nix
@@ -10,7 +10,7 @@
       background = "rgba(${config.scheme.base01-rgb-r}, ${config.scheme.base01-rgb-g}, ${config.scheme.base01-rgb-b}, 0.6)";
       border-color = "#${config.hexAccent}";
       bar-fg-color = "#${config.hexAccent}";
-      border-width = 2;
+      border-width = 0;
       time = 0.5;
     };
   };
