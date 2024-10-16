94a27050c1e25c49f505abdeb2c64b9c1b753fef
Paul Breuil <tykapl.breuil@gmail.com>
2024-10-16 16:00:03 +0200

    use nix your shell

--- modules/home-manager/fish.nix
+++ modules/home-manager/fish.nix
@@ -1,5 +1,8 @@
-{...}: {
+{pkgs, ...}: {
   programs.fish.enable = true;
+  programs.fish.interactiveShellInit = ''
+    ${pkgs.nix-your-shell}/bin/nix-your-shell fish | source
+  '';
   programs.fish.shellAliases.ls = "lsd";
   programs.fish.shellAliases.tssh = "LC_THEME=\$(cat ~/.config/current_theme) ssh -o 'SendEnv LC_THEME'";
 }
